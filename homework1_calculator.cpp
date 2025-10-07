4d13411593b6d8395d0186f1ad679cf7c283e635
pro100opt1mus <artembondarenko420@icloud.com>
2025-10-07 23:57:20 +0300

    Добавил homework1_calculator.cpp

--- homework1_calculator.cpp
+++ homework1_calculator.cpp
@@ -1,47 +1,67 @@
 #include <iostream>
-#include <iomanip>
-
+#include <stack>
+#include <sstream>
+#include <string>
 using namespace std;
 
-// Простой калькулятор на C++
-int main() {
-    char operation;
-    double num1, num2, result;
-    
-    cout << "=== ПРОСТОЙ КАЛЬКУЛЯТОР НА C++ ===" << endl;
-    cout << "Введите первое число: ";
-    cin >> num1;
-    
-    cout << "Введите оператор (+, -, *, /): ";
-    cin >> operation;
-    
-    cout << "Введите второе число: ";
-    cin >> num2;
-    
-    switch(operation) {
-        case '+':
-            result = num1 + num2;
-            break;
-        case '-':
-            result = num1 - num2;
-            break;
-        case '*':
-            result = num1 * num2;
-            break;
-        case '/':
-            if(num2 != 0) {
-                result = num1 / num2;
-            } else {
-                cout << "Ошибка: деление на ноль!" << endl;
-                return 1;
-            }
-            break;
-        default:
-            cout << "Ошибка: неверный оператор!" << endl;
-            return 1;
-    }
-    
-    cout << fixed << setprecision(2);
-    cout << "Результат: " << num1 << " " << operation << " " << num2 << " = " << result << endl;
+// Приоритет операций: '+' и '-' имеют 1, '*' и '/' – 2.
+int precedence(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
     return 0;
 }
+
+// Применяет операцию op к операндам a и b.
+double applyOp(double a, double b, char op) {
+    switch (op) {
+    case '+': return a + b;
+    case '-': return a - b;
+    case '*': return a * b;
+    case '/': return a / b;  // можно добавить проверку деления на ноль
+    default:  return 0;
+    }
+}
+
+double evaluateExpression(const string& expr) {
+    stack<double> values;  // стек для чисел
+    stack<char> ops;       // стек для операторов
+    istringstream iss(expr);
+    string token;
+
+    while (iss >> token) {
+        // Если токен – число, кладём его в стек чисел
+        if (isdigit(token[0])) {
+            values.push(stod(token));
+        }
+        else {
+            // Токен – оператор
+            char op = token[0];
+            // Выполняем операции из стека с приоритетом не ниже op
+            while (!ops.empty() && precedence(ops.top()) >= precedence(op)) {
+                double b = values.top(); values.pop();
+                double a = values.top(); values.pop();
+                char opTop = ops.top(); ops.pop();
+                values.push(applyOp(a, b, opTop));
+            }
+            ops.push(op);
+        }
+    }
+    // Выполняем оставшиеся операции
+    while (!ops.empty()) {
+        double b = values.top(); values.pop();
+        double a = values.top(); values.pop();
+        char opTop = ops.top(); ops.pop();
+        values.push(applyOp(a, b, opTop));
+    }
+    return values.top();
+}
+
+int main() {
+    string expression;
+    cout << "Введите выражение: ";
+    getline(cin, expression);
+    double result = evaluateExpression(expression);
+    cout << "Результат: " << result << endl;
+    return 0;
+}
+
